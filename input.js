b2198620b01d7a7f9a619581d4e9fb44bb6fa769
David Konsumer <konsumer@jetboystudio.com>
2022-06-20 03:07:05 -0700

    better input for RG351V

--- input.js
+++ input.js
@@ -22,6 +22,24 @@ keys:
 rshift enter
 */
 
+// controls on RG351V
+const GAMEPAD_A = 0
+const GAMEPAD_B = 1
+const GAMEPAD_X = 2
+const GAMEPAD_Y = 3
+const GAMEPAD_SELECT = 7
+const GAMEPAD_START = 6
+// const GAMEPAD_L1 = 4
+// const GAMEPAD_R1 = 5
+// const GAMEPAD_FUNCTION = 9
+// const GAMEPAD_L2 = 10
+// const GAMEPAD_R2 = 11
+
+const AXIS_DX = 6
+const AXIS_DY = 7
+const AXIS_AX = 0
+const AXIS_AY = 1
+
 module.exports = function getMappedInput (once) {
   const k = once ? r.IsKeyPressed : r.IsKeyDown
   const b = once ? r.IsGamepadButtonPressed : r.IsGamepadButtonDown
@@ -60,35 +78,55 @@ module.exports = function getMappedInput (once) {
   }
 
   if (r.IsGamepadAvailable(0)) {
-    if (b(0, r.GAMEPAD_BUTTON_RIGHT_FACE_RIGHT)) {
+    if (b(0, GAMEPAD_A)) {
       buttons.add('A')
     }
-    if (b(0, r.GAMEPAD_BUTTON_RIGHT_FACE_DOWN)) {
+    if (b(0, GAMEPAD_B)) {
       buttons.add('B')
     }
-    if (b(0, r.GAMEPAD_BUTTON_RIGHT_FACE_LEFT)) {
+    if (b(0, GAMEPAD_Y)) {
       buttons.add('Y')
     }
-    if (b(0, r.GAMEPAD_BUTTON_RIGHT_FACE_UP)) {
+    if (b(0, GAMEPAD_X)) {
       buttons.add('X')
     }
-    if (b(0, r.GAMEPAD_BUTTON_LEFT_FACE_UP)) {
-      buttons.add('UP')
-    }
-    if (b(0, r.GAMEPAD_BUTTON_LEFT_FACE_DOWN)) {
-      buttons.add('DOWN')
-    }
-    if (b(0, r.GAMEPAD_BUTTON_LEFT_FACE_LEFT)) {
-      buttons.add('LEFT')
-    }
-    if (b(0, r.GAMEPAD_BUTTON_LEFT_FACE_RIGHT)) {
-      buttons.add('RIGHT')
-    }
-    if (b(0, r.GAMEPAD_BUTTON_MIDDLE_LEFT)) {
+    if (b(0, GAMEPAD_SELECT)) {
       buttons.add('SELECT')
     }
-    if (b(0, r.GAMEPAD_BUTTON_MIDDLE_RIGHT)) {
+    if (b(0, GAMEPAD_START)) {
       buttons.add('START')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_DX) < -10) {
+      buttons.add('LEFT')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_DX) > 10) {
+      buttons.add('RIGHT')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_DY) < -10) {
+      buttons.add('UP')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_DY) > 10) {
+      buttons.add('DOWN')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_AX) < -10) {
+      buttons.add('LEFT')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_AX) > 10) {
+      buttons.add('RIGHT')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_AY) < -10) {
+      buttons.add('UP')
+    }
+
+    if (r.GetGamepadAxisMovement(0, AXIS_AY) > 10) {
+      buttons.add('DOWN')
     }
   }
 
